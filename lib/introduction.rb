5bb6809106da36072d4f0d77799e97e81c80c0a8
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-18 01:21:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,8 +10,15 @@ def introduction(name) do
 end
 
 
+def introduction_with_language(name, language) do 
+  puts "Hi, my nae is #{name} and I am learning to program in #{language}."
+end
 
 
+def introduction_with_language_optional(name, language = "Ruby) do
+  
+  puts "Hi, my nae is #{name} and I am learning to program in #{language}."
+end
 
 
 
